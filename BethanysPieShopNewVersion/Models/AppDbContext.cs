9c1570db224859ec93569160bb47c30c7b8f16ef
Attila Starkenius <symphtom@hotmail.com>
2022-12-21 07:42:06 +0100

    21.12.2022. Adding AppDbContext.cs class to BethanysPieShopNewVersion solution

---
+++ BethanysPieShopNewVersion/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShopNewVersion.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BethanysPieShopNewVersion/Program.cs
+++ BethanysPieShopNewVersion/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BethanysPieShop.Models;
+using BethanysPieShopNewVersion.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
